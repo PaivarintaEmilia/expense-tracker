560ccfeea286f6cc41d3fb5e6378d80686155f6c
Maria Emilia Päivärinta <eemilia.paivarinta@gmail.com>
2025-08-24 12:48:08 +0200

    Create styling to Header-component.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,11 +7,50 @@ import Link from 'next/link'
 
 export default function Header() {
     return (
-        <div className="border-r border-r-lime-500 border-solid h-[80%]">
-            <nav className="">
-                <Link href="/authentication">Login</Link>
-                <Link href="/home">Home</Link>
-                <button className="">Sign Out</button>
+        <div className="">
+            <nav 
+                className="
+                    flex flex-row justify-end items-center gap-5
+                    border border-sky-300 rounded-md   
+                    px-[25px] py-[10px]
+                "
+                >
+                <Link
+                    href="/authentication"
+                    className="
+                        mt-2
+                        inline-flex items-center justify-center
+                        rounded-md
+                        border border-sky-300
+                        px-4 py-2
+                        text-[15px] font-bold
+                        text-white-900
+                        mb-[3px]
+                        bg-sky-600
+                        transition
+                        hover:bg-stone-800 hover:shadow 
+                        focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sky-300
+                        active:translate-y-[1px]
+                        disabled:opacity-50 disabled:cursor-not-allowed
+                    "
+                >Login</Link>
+                <Link 
+                    href="/home"
+                    className="
+                        inline-flex items-center
+                        text-[18px] font-normal
+                        transition
+                        hover:font-bold
+                        "
+                    >Home</Link>
+                <button 
+                    className="
+                        inline-flex items-center
+                        text-[18px] font-normal 
+                        transition
+                        hover:font-bold
+                        "
+                    >Sign Out</button>
                 {/** <button onClick={() => {supabase.auth.signOut}}>Sign Out</button> */}
             </nav>
         </div>
